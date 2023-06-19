54846dd04d26a18a35a07d09864059f684a54cca
Atoulloh Salimov <atoullosalimov1811@gmail.com>
2023-06-19 19:57:41 +0300

    Added Delete, Edit and Save changes Tools and more

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
-let submit = document.querySelector('.add');
-let newTitle = document.querySelector('#new-title');
-let newBody = document.querySelector('#new-body');
-let news = 100;
+let submit = document.querySelector('.add'); // Add/Publish Button
+let newTitle = document.querySelector('#new-title'); // New title for new post
+let newBody = document.querySelector('#new-body'); // New text for new post
+let news = 0; // id of posts
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(json => {
         for(let i = 0; i < 100; i++) {
-const stylus = document.createElement("div");
+const stylus = document.createElement('div');
 stylus.className = "stylus";
 document.body.appendChild(stylus);
 
@@ -19,9 +19,76 @@ const bodyDiv = document.createElement('div');
 bodyDiv.innerText = json[i].body;
 bodyDiv.className = 'body';
 stylus.appendChild(bodyDiv);
-        }
-      });
 
+const deleteIt = document.createElement('button');
+deleteIt.innerText = 'DELETE';
+deleteIt.className = 'del';
+stylus.appendChild(deleteIt);
+
+const editItem = document.createElement('button');
+editItem.innerText = 'EDIT';
+editItem.className = 'del';
+stylus.appendChild(editItem);
+
+const saveChanges = document.createElement('button');
+saveChanges.innerText = 'SAVE';
+saveChanges.className = 'del';
+stylus.appendChild(saveChanges);
+
+// Deleting a post
+deleteIt.addEventListener('click', (delData) => {
+  location.href = '#darken2';
+  
+  const deleteItem = document.querySelector('.yes');
+  const refuse = document.querySelector('.no');
+
+  // Yes
+deleteItem.addEventListener('click', (deleteData) => {
+  news--;
+  fetch('https://jsonplaceholder.typicode.com/posts/i', {
+  method: 'DELETE',
+})
+  .then(response => response.json())
+  .then(json => {
+    stylus.remove();
+   deleteIt.remove();
+  })
+  location.href = '/index.html#';
+});
+
+// No
+refuse.addEventListener('click', (refuseIt) => {
+  location.href = '/index.html#';
+});
+
+});
+
+editItem.addEventListener('click', (changeData) => {
+  titleDiv.contentEditable = true;
+  bodyDiv.contentEditable = true;
+});
+
+saveChanges.addEventListener('click', (saveData) => {
+  titleDiv.contentEditable = false;
+  bodyDiv.contentEditable = false;
+  fetch('https://jsonplaceholder.typicode.com/posts/i', {
+    method: 'PATCH',
+    body: JSON.stringify({
+      title: titleDiv,
+      body: bodyDiv,
+      id: i
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+});
+
+news++;
+  }
+});
+
+// Create Post Button
 submit.addEventListener('click', (sendData) => {
     news++;
     fetch('https://jsonplaceholder.typicode.com/posts', {
@@ -38,7 +105,7 @@ submit.addEventListener('click', (sendData) => {
 })
   .then((response) => response.json())
   .then((json) => {
-const stylus = document.createElement("div");
+const stylus = document.createElement('div');
 stylus.className = "stylus";
 document.body.appendChild(stylus);
 
@@ -51,5 +118,71 @@ const bodyDiv = document.createElement('div');
 bodyDiv.innerText = json.body;
 bodyDiv.className = 'body';
 stylus.appendChild(bodyDiv);
-  });
+
+const deleteIt = document.createElement('button');
+deleteIt.innerText = 'DELETE';
+deleteIt.className = 'del';
+stylus.appendChild(deleteIt);
+
+const editItem = document.createElement('button');
+editItem.innerText = 'EDIT';
+editItem.className = 'del';
+stylus.appendChild(editItem);
+
+const saveChanges = document.createElement('button');
+saveChanges.innerText = 'SAVE';
+saveChanges.className = 'del';
+stylus.appendChild(saveChanges);
+
+// Deleting a post
+deleteIt.addEventListener('click', (delData) => {
+  location.href = '#darken2';
+  
+  const deleteItem = document.querySelector('.yes');
+  const refuse = document.querySelector('.no');
+
+  // Yes
+deleteItem.addEventListener('click', (deleteData) => {
+  news--;
+  fetch('https://jsonplaceholder.typicode.com/posts/i', {
+  method: 'DELETE',
+})
+  .then(response => response.json())
+  .then(json => {
+    stylus.remove();
+   deleteIt.remove();
+  })
+  location.href = '/index.html#';
+});
+
+// No
+refuse.addEventListener('click', (refuseIt) => {
+  location.href = '/index.html#';
+});
+
+});
+
+editItem.addEventListener('click', (changeData) => {
+  titleDiv.contentEditable = true;
+  bodyDiv.contentEditable = true;
+});
+
+saveChanges.addEventListener('click', (saveData) => {
+  titleDiv.contentEditable = false;
+  bodyDiv.contentEditable = false;
+  fetch('https://jsonplaceholder.typicode.com/posts/i', {
+    method: 'PATCH',
+    body: JSON.stringify({
+      title: titleDiv,
+      body: bodyDiv,
+      id: i
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+});
+
+news++;
+  })
 });
